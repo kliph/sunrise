95b7f10bac272ca30d0e284f28ee55c8dad67536
Cliff Rodgers <kliph@users.noreply.github.com>
2017-11-20 21:00:46 -0500

    Add simple skybox

--- src/sunrise/core.cljs
+++ src/sunrise/core.cljs
@@ -5,7 +5,25 @@
 (def by-id goog.dom.getElement)
 
 (defn app-container []
-  [:h1 {}
-   "Hello World"])
+  [:svg {:xmlns "http://www.w3.org/2000/svg"
+         :width "100%"
+         :height "100%"}
+   [:defs {}
+    [:linearGradient#skyGradient {:x1 "0"
+                                  :y1 "0"
+                                  :x2 "0"
+                                  :y2 "1"}
+     [:stop {:className "stop1"
+             :offset "0%"
+             :stop-opacity "0"}]
+     [:stop {:className "stop2"
+             :offset "100%"}]
+     ]]
+   [:g.skybox {}
+    [:rect {:width "100%"
+            :className "sky"
+            :height "50%"
+            :y "0"
+            :x "0"}]]])
 
 (r/render-component [app-container] (by-id "app"))
