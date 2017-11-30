94f702cc1a108617ad353d1c750ae3ffedcd45da
Ryan Hinkel <ryanhinkel@gmail.com>
2017-11-30 17:19:36 -0500

    Move components into separate namespace

--- src/sunrise/core.cljs
+++ src/sunrise/core.cljs
@@ -1,85 +1,13 @@
 (ns sunrise.core
-  (:require [reagent.core :as r]))
+  (:require [reagent.core :as reagent]
+            [sunrise.component :as component]))
 
 (defn get-window-dimensions []
   (let [width (.-innerWidth js/window)
         height (.-innerHeight js/window)]
     {:width width :height height}))
 
-(def display-state (r/atom {:window (get-window-dimensions)}))
+(def display-state (reagent/atom {:window (get-window-dimensions)}))
 
 (.addEventListener js/window "resize" #(swap! display-state assoc :window (get-window-dimensions)))
-
-(defn line-dy [base]
-  (- (rand base)
-     (/ base 2)))
-
-(defn line-dx [base]
-  (/ 25 2))
-
-(defn line-delta [dx dy]
-  (str "l " dx " " dy))
-
-(defn ridge-line [n base]
-  (let [dys (->> (repeatedly (* 2 n) #(line-dy base))
-                 (partition 2)
-                 (map (fn [[a b]]
-                        (* 0.5 (+ a b)))))
-        dxs (repeatedly n #(line-dx base))
-        line-samples (map line-delta dxs dys)]
-    (clojure.string/join " " line-samples)))
-
-(defn mountain-path [n width height base nth-mountain]
-  (let [y (+ (* height 0.4) (* nth-mountain 30))]
-    (str "M 0 "
-         y
-         (ridge-line n base)
-         "L " width " "
-         y
-         "V " height
-         "L 0 " height
-         "Z")))
-
-(defn mountains [n width height base n-mountains]
-  (map
-   (fn [x]
-     (let [name (str "mountain-" x)]
-       [:path
-        {:className name
-         :key name
-         :d (mountain-path n width height base x)
-         :opacity 0.5}]))
-   (range n-mountains)))
-
-(defn app-container []
-  (let [state @display-state
-        width (get-in state [:window :width])
-        height (get-in state [:window :height])
-        n 60
-        base 10
-        n-mountains 4]
-    [:svg {:xmlns "http://www.w3.org/2000/svg"
-            :width "100%"
-            :height "100%"}
-      [:defs {}
-      [:linearGradient#skyGradient {:x1 "0"
-                                    :y1 "0"
-                                    :x2 "0"
-                                    :y2 "1"}
-        [:stop {:className "stop1"
-                :offset "0%"
-                :stop-opacity "0"}]
-        [:stop {:className "stop2"
-                :offset "60%"}]]]
-      [:g.skybox {}
-      [:rect.sky {:width "100%"
-                  :height "100%"
-                  :y "0"
-                  :x "0"}]
-      [:circle.sun {:r "10%"
-                    :cx "40%"
-                    :cy "25%"}]]
-      [:g.foreground {}
-        (mountains n width height base n-mountains)]]))
-
-(r/render-component [app-container] (.getElementById js/document "app"))
+(reagent/render-component [component/app-container] (.getElementById js/document "app"))
