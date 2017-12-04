b510359be6a9ed905b23441ab8e051eaa0c2c64e
Ryan Hinkel <ryanhinkel@gmail.com>
2017-12-04 18:28:09 -0500

    Do not concern your state with the global window object

--- src/sunrise/core.cljs
+++ src/sunrise/core.cljs
@@ -1,8 +1,17 @@
 (ns sunrise.core
   (:require [reagent.core :as reagent]
             [sunrise.component :as component]
-            [sunrise.state]))
+            [sunrise.state :as state]))
 
-(.addEventListener js/window "resize" sunrise.state/update!)
+(defn get-window-dimensions []
+  (let [width (.-innerWidth js/window)
+        height (.-innerHeight js/window)]
+    {:width width :height height}))
 
+(defn update-window-state
+  (state/update-window! (get-window-dimensions)))
+
+(.addEventListener js/window "resize" update-window-state)
+
+(update-window-state)
 (reagent/render-component [component/app-container] (.getElementById js/document "app"))
--- src/sunrise/state.cljs
+++ src/sunrise/state.cljs
@@ -1,12 +1,7 @@
 (ns sunrise.state
   (:require [reagent.core :as reagent]))
 
-(defn update! []
-  (swap! sunrise.state/display-state assoc :window (sunrise.state/get-window-dimensions)))
+(def display-state (reagent/atom {}))
 
-(defn get-window-dimensions []
-  (let [width (.-innerWidth js/window)
-        height (.-innerHeight js/window)]
-    {:width width :height height}))
-
-(def display-state (reagent/atom {:window (get-window-dimensions)}))
+(defn update-window! [dimensions]
+   (swap! display-state assoc :window dimensions))
