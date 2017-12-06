216b923cb15ffb687a07b1c579ca425b865e2ed2
Cliff Rodgers <kliph@users.noreply.github.com>
2017-12-05 19:20:29 -0500

    Extract mountains into their own component
    - Further closes #2

--- src/sunrise/component.cljs
+++ src/sunrise/component.cljs
@@ -1,47 +1,7 @@
 (ns sunrise.component
   (:require [reagent.core :as reagent]
-            [sunrise.state]))
-
-(defn line-dy [base]
-  (- (rand base)
-     (/ base 2)))
-
-(defn line-dx [base]
-  (/ 25 2))
-
-(defn line-delta [dx dy]
-  (str "l " dx " " dy))
-
-(defn ridge-line [n-points base]
-  (let [dys (->> (repeatedly (* 2 n-points) #(line-dy base))
-                 (partition 2)
-                 (map (fn [[a b]]
-                        (* 0.5 (+ a b)))))
-        dxs (repeatedly n-points #(line-dx base))
-        line-samples (map line-delta dxs dys)]
-    (clojure.string/join " " line-samples)))
-
-(defn mountain-path [n-points width height base nth-mountain]
-  (let [y (+ (* height 0.4) (* nth-mountain 30))]
-    (str "M 0 "
-         y
-         (ridge-line n-points base)
-         "L " width " "
-         y
-         "V " height
-         "L 0 " height
-         "Z")))
-
-(defn mountains [n-points width height base n-mountains]
-  (map
-   (fn [x]
-     (let [name (str "mountain-" x)]
-       [:path
-        {:className name
-         :key name
-         :d (mountain-path n-points width height base x)
-         :opacity 0.5}]))
-   (range n-mountains)))
+            [sunrise.state]
+            [sunrise.mountain :as mountain]))
 
 (defn app-container []
   (let [state @sunrise.state/display-state
@@ -75,4 +35,4 @@
                     :cx "40%"
                     :cy "25%"}]]
      [:g.foreground {}
-      (mountains n-points width height base n-mountains)]]))
+      (mountain/mountains n-points width height base n-mountains)]]))
